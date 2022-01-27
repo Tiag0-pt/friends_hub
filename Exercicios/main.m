31a69a2ca54f4b31cb82e0fc3d24a6603607ed54
Tiago <tiago_portugal@ua.pt>
2022-01-27 21:27:24 +0000

    made option 1, some doubts in the second thought

--- Exercicios/main.m
+++ Exercicios/main.m
@@ -1,19 +1,36 @@
 users = readcell('users.txt','Delimiter',';');
-friends = readcell('friends.txt','Delimiter',';');
+friends = readcell('friends.txt','Delimiter',' ');
 
-str = input("Insert Valid user ID:\n");
-ID = str2num(str);
+interesses = cell(1,length(users));
+for i=1:length(users)
+    cell_i = users(i,4:end);
+    cell_i( cellfun( @(cell_i) isa(cell_i,'missing'), cell_i ) ) = {[]};
+    interesses{i} = cell_i;
+end
+
+ID = input("Insert Valid user ID:\n\n");
+
 while 1
-    str = input("\n1- Your friends\n 2- Interest from most similar user \n3- Search Name\n4- Find most similar users based in list of interests\n5-Exit\nSelect choice:\n");
-    opt = str2num(str);
+    opt = input("\n1- Your friends\n2- Interest from most similar user \n3- Search Name\n4- Find most similar users based in list of interests\n5-Exit\nSelect choice:\n\n");
+    
     
     switch opt
-        case 1:
-            for i=0:length(friends)
-                if(friends{1})
-                
-                
+        case 1
+            fprintf("\n");
+            for i=1:length(friends)
+                if(friends{i,1} == ID)
+                    fprintf("Id: %d Nome: %s %s\n",friends{i,2},users{friends{i,2},2},users{friends{i,2},3});
+                    if(friends{i+1,1}~=ID)
+                        break;
+                    end
+                end
             end
+            
+        case 2
+            
+            
+    end
+                
             
             
 end
